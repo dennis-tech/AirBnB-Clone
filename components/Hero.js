6cd15d89d91a9864b276846899c0aa188d53a9bb
dennis-tech <hedenniskibe@gmail.com>
2023-11-06 22:34:46 +0300

    created hero component_

---
+++ components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section>
+        <section className="hero">
+            <img src="../images/photo-grid.png" className="hero__photo" />
+            <h1 className="hero__header">Online Experiences</h1>
+            <p className="hero__text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    </section>
+  )
+}
